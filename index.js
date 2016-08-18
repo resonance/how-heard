77df693919f71bf22df8ee26e77518aef800f94f
Elizabeth Coletta <elizabethcoletta@Elizabeths-Air.fios-router.home>
2016-08-18 11:18:20 -0400

    added all beginning npm packages

--- index.js
+++ index.js
@@ -6,6 +6,16 @@
 
 var fs = require('fs');
 var koa = require('koa');
+var koaRouter = require('koa-router');
+var bodyParser = require('koa-bodyparser');
+var serve = require('koa-static');
+var cors = require('kcors');
+var jade = require('jade');
+var mount = require('koa-mount');
+var request = require('request');
+var thunkify = require('thunkify');
+var moment = require('moment-timezone');
+
 
 var app = koa();
 
