bdc2664e188ef8b0fc718487250f9af532cc497b
Elizabeth Coletta <elizabethcoletta@Elizabeths-Air.fios-router.home>
2016-08-18 11:35:45 -0400

    initialized packages, mounted admin, added error handling

--- index.js
+++ index.js
@@ -15,20 +15,97 @@ var mount = require('koa-mount');
 var request = require('request');
 var thunkify = require('thunkify');
 var moment = require('moment-timezone');
+var admin = require('./admin');
 
+
+
+
+/**
+ * Load jade template files.
+ */
+
+
+
+/**
+ * Create mini-app.
+ */
 
 var app = koa();
+
+var post = thunkify(request.post);
+var get = thunkify(request.get);
+
+
+module.exports = app;
+
+
+
+/**
+ * Initialize all needed middleware packages
+ */
+
+app.use(cors());
+app.use(bodyParser());
+var router = koaRouter();
+app.use(router.routes());
+
+
+
+/**
+ * Mount other apps as middleware
+ */
+
+app.use(mount('/admin', admin));
+
+
+
+
+/**
+ * Publicly expose static assets.
+ */
+
+app.use(mount('/public', serve(__dirname+'/public')));
+
+
+
+
+
+/**
+ * Error handling.
+ */
+
+app.use(function *(next) {
+  try {
+    yield next;
+  } catch(err) {
+    this.status = err.status || 500;
+    this.body = 'Sorry, we made a mistake.';
+    this.app.emit('error', err, this);
+  }
+});
+
+
+
+
 
 app.use(function *(){
 	this.body="hi mom";
 	}	
 )
 
+
+
+
 var port = process.env.PORT || 4000;
+
 
 app.listen(port,function() {
 console.log("mjk-test up and running on",port);
 }
 );
+
+
+
+
 
 
