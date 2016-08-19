b5662657c41f2c31eafddc64c204f98bcde47f4e
Elizabeth Coletta <elizabethcoletta@Elizabeths-Air.fios-router.home>
2016-08-19 16:00:47 -0400

    added cors.js for cross-origin support

---
+++ public/cors.js
@@ -0,0 +1,21 @@
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+
+var url = 'https://www.tuckernyc.com';
+var xhr = createCORSRequest('GET', url);
+xhr.send();
