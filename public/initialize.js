cca62f3f7992d41a37f0b90f9a9ceb536b43d32c
Elizabeth Coletta <elizabethcoletta@Elizabeths-MacBook-Air.local>
2016-08-19 19:21:39 -0400

    moved jquery append with iframe to top of code

--- public/initialize.js
+++ public/initialize.js
@@ -11,12 +11,12 @@
 
 
 !function() {
-	
+  $('#cross').append('<iframe id="howhearddropdown" width="0" height="0" style="border:none;"></iframe>');	
   var iframeId = 'howhearddropdown'
   var iframe = document.getElementById(iframeId);
   iframe.src = 'https://how-heard.herokuapp.com/dropdown?key=0';
   iframe.width = '100%';
   iFrameResize(undefined, iframe);
   
-  $('#cross').append('<iframe id="howhearddropdown" width="0" height="0" style="border:none;"></iframe>');
+  
 }()
