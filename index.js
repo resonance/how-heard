f834b97079f5363286e952ec8a21c30b1b656056
Elizabeth Coletta <elizabethcoletta@Elizabeths-MacBook-Air.local>
2016-08-25 11:00:04 -0400

    fixed refactoring in /

--- index.js
+++ index.js
@@ -208,12 +208,12 @@ router.get('/', function *() {
 
 
   // get existing list of howheards if available, use shopsCollection object
-  const howHeardList = yield howHeard.findHowHeardList(shop.companyName);
+  const howHeardList = yield howHeard.findHowHeardList(shop);
 
   // if list exists, add to jadeOptions
   if (howHeardList) {
 	
-	const list = yield howHeard.getHowHeardList(shop.companyName);
+	const list = yield howHeard.getHowHeardList(shop);
 	
 	jadeOptions.choices = {
       selections: list.selections
