23e301e9ae769b081d9e384c1dd7060db3cd3849
Elizabeth Coletta <elizabethcoletta@Elizabeths-Air.home>
2016-08-31 22:26:14 -0400

    replacing storeName wtih storeId

--- index.js
+++ index.js
@@ -651,7 +651,7 @@ router.post('/messages/:shopName/:type', function *() {
 router.get('/reporting', function *() {
   
   // find store object in db
-  const shop = yield howHeard.findShop(this.query.shop);
+  const shop = yield howHeard.findShop(this.query.storeId);
   const shopName = shop.companyName;
 
   const orders = yield howHeard.fetchStoreOrders(shopName);
